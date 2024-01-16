d3c8015480739cf90fe9b618e0ae49bf08a50a54
sprajeswar <98775825+sprajeswar@users.noreply.github.com>
2024-01-16 15:32:01 -0500

    emojis streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 
 streamlit.title('My another trial in Streamlit!!!')
 streamlit.header("This is header")
-streamlit.text('line 1')
-streamlit.text('line 2')
-streamlit.text('line 3')
+streamlit.text('🥣line 1')
+streamlit.text('🥗line 2')
+streamlit.text('🐔line 3')
+streamlit.text('🥑line 3')
+streamlit.text('🍞line 3')
