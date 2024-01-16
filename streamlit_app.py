9c45abfb2eb87fe427d81cb5d805082deed5deb4
sprajeswar <98775825+sprajeswar@users.noreply.github.com>
2024-01-16 15:47:49 -0500

    with pandas streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
-import streamlit
+#import streamlit
 
-streamlit.title('My another trial in Streamlit!!!')
-streamlit.header("This is header")
-streamlit.text('🥣line 1')
-streamlit.text('🥗line 2')
-streamlit.text('🐔line 3')
-streamlit.text('🥑line 3')
-streamlit.text('🍞line 3')
+#streamlit.title('My another trial in Streamlit!!!')
+#streamlit.header("This is header")
+#streamlit.text('🥣line 1')
+#streamlit.text('🥗line 2')
+#streamlit.text('🐔line 3')
+#streamlit.text('🥑line 3')
+#streamlit.text('🍞line 3')
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
