1ba87f49551d0e11c8cbe2af2b49a05cb40d3af9
sprajeswar <98775825+sprajeswar@users.noreply.github.com>
2024-01-16 15:28:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
 streamlit.title('My another trial in Streamlit!!!')
-streamlit.title('Multiline streamlit!')
+streamlit.header("This is header")
+streamlit.text('line 1')
+streamlit.text('line 2')
+streamlit.text('line 3')
